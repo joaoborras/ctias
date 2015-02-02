f4a9aa5ef11cb2c048030d59609b965817881ded
joaoborras <jpekko@Joao-no-MacBook-Pro.local>
2015-02-02 15:52:02 +0900

    included a test text in app.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 //**************** NewRelic monitoring ***********************
 //require('newrelic');
 
+this is a test!
+
 //*********************** variables and objects used in the proxy ******************
 //no final, o object credentials sera um array porque vai ter varias ZD's apps conectando no proxy
 var credentials = [{
